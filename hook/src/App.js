51d984911f7d96f791fc485c65a4d4e367e6a7f3
John Clark <clark2j2@johnmclark.com>
2020-09-16 10:58:46 -0400

    First Commit

---
+++ hook/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './App.css';
+import Form from './components/form'
+import Results from './components/results'
+
+function App() {
+  const [state, setState] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+  return (
+    <div className="App">
+      <Form inputs={state} setInputs={setState} />
+      <Results data={state} />
+    </div>
+  );
+}
+
+export default App;
